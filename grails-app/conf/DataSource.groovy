a3306414a9631c12929f5e4a67783b243000e2ea
Janusz Slota <janusz.slota@nixilla.com>
2013-02-01 20:23:38 +0000

    Added config for Heroku

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,33 +1,36 @@
 dataSource {
-	pooled = true
-        url = "jdbc:mysql://192.168.1.50:3306/ois"
-	driverClassName = "com.mysql.jdbc.Driver"
-	username = "ois"
-	password = "ois"
+    pooled = true
+    url = "jdbc:mysql://localhost:3306/ois"
+    driverClassName = "com.mysql.jdbc.Driver"
+    username = "root"
+    password = ""
 }
 hibernate {
-    cache.use_second_level_cache=true
-    cache.use_query_cache=true
-    cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
+    cache.use_second_level_cache = true
+    cache.use_query_cache = true
+    cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
-	development {
-		dataSource {
-			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-			url = "jdbc:mysql://192.168.1.50/oisdev"
-		}
-	}
-	test {
-		dataSource {
-			dbCreate = "update"
-			url = "jdbc:mysql://192.168.1.50/oistest"
-		}
-	}
-	production {
-		dataSource {
-			dbCreate = "update"
-			url = "jdbc:mysql://192.168.1.50/oislive"
-		}
-	}
+    development {
+        dataSource {
+            dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+            url = "jdbc:mysql://dev:3306/oisdev"
+        }
+    }
+    test {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:mysql://localhost:3306/oistest"
+        }
+    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            uri = new URI(System.env.CLEARDB_DATABASE_URL)
+            url = "jdbc:mysql://" + uri.host + uri.path
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
+        }
+    }
 }
